55ca6355d83046f02a58737b29a64c6bf04fa2f5
vikvas <vikvas6@ya.ru>
2021-03-03 22:09:33 +0300

    Lesson 2 HW

--- GB_Unity2D/Assets/Scripts/Lessons.cs
+++ GB_Unity2D/Assets/Scripts/Lessons.cs
@@ -8,20 +8,34 @@ public class Lessons : MonoBehaviour
     [SerializeField] private Camera _camera;
     [SerializeField] private CharacterView _characterView;
     [SerializeField] private SpriteRenderer _back;
+    [SerializeField] private GunView _gun;
+    
 
     //[SerializeField] private SomeView _someView;
-    [SerializeField] private SpriteAnimator _spriteAnimator;
+    [SerializeField] private SpriteAnimatorController _spriteAnimator;
+    [SerializeField] private int _animationSpeed = 10;
+
+    private MainHeroWalker _hero;
+    private AimingMuzzle _muzzle;
+    private BulletsEmitter _bulletEmitter;
     
     private void Start()
     {
         SpriteAnimationsConfig config = Resources.Load("SpriteAnimationConfig", typeof(SpriteAnimationsConfig)) as SpriteAnimationsConfig;
-        _spriteAnimator = new SpriteAnimator(config);
-        _spriteAnimator.StartAnimation(_characterView.SpriteRenderer, Track.Run, true, 10);
+        _spriteAnimator = new SpriteAnimatorController(config);
+        _spriteAnimator.StartAnimation(_characterView.SpriteRenderer, Track.Run, true, _animationSpeed);
+        _hero = new MainHeroWalker(_characterView, _spriteAnimator);
+        
+        _muzzle = new AimingMuzzle(_gun._muzzleTransform, _characterView.transform);
+        _bulletEmitter = new BulletsEmitter(_gun._bullets, _gun._emitterTransform);
     }
 
     private void Update()
     {
         _spriteAnimator.UpdateTick();
+        _hero.UpdateTick();
+        _muzzle.UpdateTick();
+        _bulletEmitter.UpdateTick();
     }
 
     private void FixedUpdate()
--- GB_Unity2D/Assets/Scripts/Views/CharacterView.cs
+++ GB_Unity2D/Assets/Scripts/Views/CharacterView.cs
@@ -5,7 +5,11 @@ public class CharacterView : MonoBehaviour
 {
     #region Fields
 
+    public Transform Transform;
     public SpriteRenderer SpriteRenderer;
+    public Rigidbody2D Rigidbody2D;
+    public Collider2D Collider2D;
+    public TrailRenderer Trail;
 
     #endregion
 }
