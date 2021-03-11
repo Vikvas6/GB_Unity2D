3001c447ef6ae40bfe7bc449fd180435e76ece3d
vikvas <vikvas6@ya.ru>
2021-03-11 15:08:00 +0300

    Lesson 4 HW

---
+++ GB_Unity2D/Assets/Scripts/Controllers/ElevatorManager.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class ElevatorManager : IDisposable
+{
+    private List<CharacterView> _elevatorViews;
+    private List<CharacterView> _limitViews;
+    
+    public ElevatorManager(List<CharacterView> elevatorViews, List<CharacterView> limitViews)
+    {
+        _elevatorViews = elevatorViews;
+        _limitViews = limitViews;
+
+        for (int i = 0; i < _limitViews.Count; i++)
+        {
+            _limitViews[i].OnLevelObjectContact += TurnElevator;
+        }
+    }
+
+    public void Dispose()
+    {
+        for (int i = 0; i < _limitViews.Count; i++)
+        {
+            _limitViews[i].OnLevelObjectContact -= TurnElevator;
+        }
+    }
+
+    private void TurnElevator(CharacterView other)
+    {
+        if (_elevatorViews.Contains(other))
+        {
+            var motor = other.GetComponent<SliderJoint2D>().motor;
+            motor.motorSpeed = -motor.motorSpeed;
+            other.GetComponent<SliderJoint2D>().motor = motor;
+        }
+    }
+}
